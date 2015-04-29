afb996aacbbfda3fe9a6bfd50f7ac08ba900ae7a
张科 <zhangke19880503@163.com>
2015-04-29 18:26:34 +0800

    Initial Commit

---
+++ ARC Effective/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARC Effective
+//
+//  Created by zhangke on 15/4/29.
+//  Copyright (c) 2015年 zhangke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ARC Effective/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ARC Effective
+//
+//  Created by zhangke on 15/4/29.
+//  Copyright (c) 2015年 zhangke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
