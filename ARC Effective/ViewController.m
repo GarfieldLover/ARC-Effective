126703db75189700a81c7cb5230773f81574e857
张科 <zhangke19880503@163.com>
2015-04-29 23:52:38 +0800

    __autoreleasing 内存 arc

--- ARC Effective/ViewController.m
+++ ARC Effective/ViewController.m
@@ -26,6 +26,17 @@
     NSLog(@"2 %p",obj);
     NSLog(@"3 %p",temp);
     NSLog(@"4 %p",temp);
+    
+    
+    id __strong obj1=[[NSObject alloc] init];
+    
+    @autoreleasepool {
+        //arc  __autoreleasing 计数＋1
+        //weak 不＋1
+        id __autoreleasing o=obj1;
+//        id __weak o=obj1;
+
+    }
 
 }
 
